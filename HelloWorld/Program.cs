26d10053524b2c271018091cabe828193067f43e
Jonas <admin@AR-113-05>
2017-08-30 08:54:26 +0200

    Start på dag 2

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast navn");
+            Console.WriteLine("Velkommen " + Console.ReadLine().ToUpper());
+            Console.WriteLine("Tryk på en tast for at fortsætte");
+            Console.ReadKey();
+        }
+    }
+}
--- Module03_ValueTypes/Program.cs
+++ Module03_ValueTypes/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace Module03_ValueTypes
+﻿using System;
+
+namespace Module03_ValueTypes
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
+            //System.Int32 i;
+            //System.Decimal komma;
+            int i;
+            decimal komma;
+            DateTime dato = new DateTime(2017, 08, 29);
+            dato = DateTime.Today;
+            
+
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
