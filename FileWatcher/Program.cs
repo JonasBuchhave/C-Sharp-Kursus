5695e3dcb32c1b543924b49e8c7b4f9dffd97085
JonasBuchhave <jonas@buchhave.net>
2017-10-10 16:08:19 +0200

    Kursus slut!

---
+++ FileWatcher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.IO.FileSystemWatcher w = new System.IO.FileSystemWatcher(@"c:\temp", "*.txt");
+            w.EnableRaisingEvents = true;
+            w.NotifyFilter = System.IO.NotifyFilters.LastWrite | System.IO.NotifyFilters.FileName;
+
+            w.Changed += (s, e) => { Console.WriteLine("Filen " + e.Name + " er rettet"); };
+            w.Renamed += (s, e) => { Console.WriteLine("Filen " + e.OldName + " er omdøbt til " + e.Name); };
+
+
+            do { } while (true);
+        }
+    }
+}
---
+++ GeneriskMetode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneriskMetode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 16;
+            int y = 32;
+            Console.WriteLine(x + " " + y);
+            Swap(ref x, ref y);
+            Console.WriteLine(x + " " + y);
+
+            Console.ReadKey();
+        }
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+}
